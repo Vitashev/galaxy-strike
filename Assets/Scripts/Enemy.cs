83c148f6337fded47f08715156aa341356baac7c
Vitalii Shevchuk <vitashevc@gmail.com>
2025-06-24 00:26:29 -0700

    add prefab, fix bugs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private GameObject explosionEffect; // Speed of the enemy movement
     void OnParticleCollision(GameObject other)
     {
+        Debug.Log("Enemy hit by a particle!");
+        Instantiate(explosionEffect, transform.position, Quaternion.identity); // Spawn the explosion effect
         Destroy(gameObject); // Destroy the enemy when hit by a particle
+
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
 
-public class CollisionHandler : MonoBehaviour
+public class PlayerCollision : MonoBehaviour
 {
+    [SerializeField] private GameObject explosionEffect;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enemy hit by a particle!");
+        Instantiate(explosionEffect, transform.position, Quaternion.identity); // Spawn the explosion effect
+        Destroy(gameObject); // Destroy the enemy when hit by a particle
+        Debug.Log("Trigger detected with: " + other.gameObject.name);
+    }
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collision detected with: " + collision.gameObject.name);
     }
+
 
     void Start()
     {
